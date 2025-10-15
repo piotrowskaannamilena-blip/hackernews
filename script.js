721599902954017d2e3061f9b67c0eb4cc3fa938
Burcu Gunes <burcudagli.10@gmail.com>
2025-10-15 13:46:42 +0100

    Added Js functions to fetch data

---
+++ script.js
@@ -0,0 +1,33 @@
+var apiKey = "Your_Api_Key";
+var url = `https://newsapi.org/v2/everything?q=Apple&sortBy=popularity&apiKey=${apiKey}`;
+var newsDiv=document.getElementById("news-container-div");
+
+  async function getNewsData() {
+    try {
+      let response = await fetch(url);
+      let data = await response.json();
+      return data.articles;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  function createNewArticleDiv(article){
+    var articleDiv=document.createElement("div");
+            articleDiv.innerHTML = 
+            `<h3>${article.title}</h3>
+            <img src="${article.urlToImage}">
+            <p>${article.description}</p>
+            <p>Published :${article.publishedAt}</p>
+            <a href="${article.url}" >Read More</a>`;
+            
+            
+            newsDiv.appendChild(articleDiv);
+};
+
+async function showNews() {
+  let articles = await getNewsData();
+  articles.forEach(article => createNewArticleDiv(article));
+};
+
+showNews();
+
